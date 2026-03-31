57e6ec150be7c552bb3805343c120fd55652c58f
Павел Молев <onvon5404@gmail.com>
2026-03-31 23:53:26 +0300

    1lab_update

---
+++ Magazine.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Magazine.Core.Models;
+
+namespace Magazine.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductService _service = new ProductService();
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _service.Add(product);
+            return Ok(result);
+        }
+
+        [HttpDelete("remove/{id}")]
+        public IActionResult Remove(Guid id)
+        {
+            var result = _service.Remove(id);
+            return Ok(result);
+        }
+
+        [HttpPut("edit")]
+        public IActionResult Edit(Product product)
+        {
+            var result = _service.Edit(product);
+            return Ok(result);
+        }
+
+        [HttpGet("search")]
+        public IActionResult Search(string name)
+        {
+            var result = _service.Search(name);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+    }
+}
